4561c661935b31a44133d0c11e3d34e00f862a31
Papanya Siriwattanaworakul <papanya2546@gmail.com>
2025-03-29 23:46:40 +0700

    remove test endpoint

--- store-backend/src/api/category.ts
+++ store-backend/src/api/category.ts
@@ -4,15 +4,7 @@ import db from '../db'
 const router = Router()
 
 router
-    .get('/test', (req, res) => {
-        res.json([
-            { id: 1, name: 'Electronics' },
-            { id: 2, name: 'Books' },
-            { id: 3, name: 'Clothing' },
-            { id: 4, name: 'Sports' },
-        ])
-    })
-    .get('/db', async (req, res) => {
+    .get('/', async (req, res) => {
         const data = await db('category').select('*').orderBy('id')
         console.log(req.body)
         res.json(data) 
