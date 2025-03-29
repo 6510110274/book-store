b8123981a9bab1943a5049722d21050b75aab35c
Papanya Siriwattanaworakul <papanya2546@gmail.com>
2025-03-29 17:51:57 +0700

    add knex configuration and initial migration for category table

---
+++ store-backend/migrations/init.js
@@ -0,0 +1,16 @@
+// knex migrate:up
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('category', function (table) {
+        table.integer('id').unsigned().primary(); // ใช้ integer ธรรมดา เพราะคุณใส่ id เอง
+        table.string('title', 100).notNullable();
+      })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTable('category');
+  };
+  
+  exports.config = { transaction: false };
+  
